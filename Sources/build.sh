96921083217b8d01611765fd0a26956152f1ea37
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2020-05-29 08:21:50 -0500

    new build, sources, and font files

--- Sources/build.sh
+++ Sources/build.sh
@@ -1,12 +1,12 @@
 # source venv/bin/activate
+set -e
 
-mkdir -p ../fonts ../fonts/ttfs ../fonts/otf ../fonts/variable
-# VF_FILE=../fonts/variable/Varta\[wght]\.ttf
+mkdir -p ../fonts ../fonts/ttfs ../fonts/otf ../fonts/variable ../fonts/woff2
 
-# Generating TTFs
+echo "GENERATING TTFs"
 fontmake -g Varta.glyphs -i -o ttf --output-dir ../fonts/ttfs/
 
-# Post processing TTFs
+echo "POST PROCESSING TTFs"
 ttfs=$(ls ../fonts/ttfs/*.ttf)
 for ttf in $ttfs
 do
@@ -19,23 +19,35 @@ do
     gftools fix-dsig -f $ttf;
 done
 
-# # Generating OTFs
-# fontmake -g Varta.glyphs -i -o otf --output-dir ../fonts/otf/
+echo "GENERATING WOFFs"
+ttfs=$(ls ../fonts/*/*.ttf)
+for ttf in $ttfs; do
+    woff2_compress $ttf
+done
 
-# # Post processing OTFs
-# otfs=$(ls ../fonts/otf/*.otf)
-# for otf in $otfs
-# do
-#     gftools fix-dsig -f $otf;
-# done
+woff2s=$(ls ../fonts/*/*.woff2)
+for woff2 in $woff2s; do
+    mv $woff2 ../fonts/woff2/$(basename $woff2)
+done
 
-# Generating VFs
+
+echo "GENERATING OTFs"
+fontmake -g Varta.glyphs -i -o otf --output-dir ../fonts/otf/
+
+echo "POST PROCESSING OTFs"
+otfs=$(ls ../fonts/otf/*.otf)
+for otf in $otfs
+do
+    gftools fix-dsig -f $otf;
+done
+
+echo "GENERATING VFs"
 VF_FILE=../fonts/variable/Varta\[wght]\.ttf
 fontmake -g Varta.glyphs -o variable --output-path $VF_FILE
 
 rm -rf master_ufo/ instance_ufo/
 
-# Post processing VFs
+echo "POST PROCESSING VFs"
 ttfautohint $VF_FILE $VF_FILE.fix
 mv $VF_FILE.fix $VF_FILE
 
@@ -45,3 +57,5 @@ mv $VF_FILE.fix $VF_FILE
 gftools fix-dsig -f $VF_FILE
 
 gftools fix-unwanted-tables $VF_FILE -t MVAR
+
+python3 varta-stat-table2.py $VF_FILE
